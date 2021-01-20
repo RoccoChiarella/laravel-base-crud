37dacbba6398522fc9e111cd7ed87e11d20286b4
Rocco Chiarella <rocco.chiarella.rc@gmail.com>
2021-01-20 16:02:53 +0100

    Cambiato nome file, perchè non coerente con lo standard

--- database/migrations/2021_01_20_145430_create_products_table.php
+++ database/migrations/2021_01_20_145430_create_products_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateTableProducts extends Migration
+class CreateProductsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateTableProducts extends Migration
      */
     public function up()
     {
-        Schema::create('table_products', function (Blueprint $table) {
+        Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->float('price');
@@ -29,6 +29,6 @@ class CreateTableProducts extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('table_products');
+        Schema::dropIfExists('products');
     }
 }
