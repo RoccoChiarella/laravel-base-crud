d99598f184255b2d7a4afc919a806a71749d20c2
Rocco Chiarella <rocco.chiarella.rc@gmail.com>
2021-01-20 16:53:53 +0100

    Passaggio dei valori della tabella e stampa con un foreach

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -5,7 +5,12 @@
         <div class="row">
             <div class="col-12">
                 <h1 class="text-center">Tutti i prodotti</h1>
+                @foreach ($products as $product)
+                    <p>{{ $product->name }}</p>
+                @endforeach
             </div>
         </div>
     </div>
 @endsection
+
+@section('page-title', 'Products - Laravel Crud')
