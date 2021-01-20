25dc0911476f49af8722f2fdc30d72b63c05f022
Rocco Chiarella <rocco.chiarella.rc@gmail.com>
2021-01-20 16:39:55 +0100

    Installato bootstrap + navbar header

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,13 @@
 @extends('layouts.app')
 
 @section('content')
-
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <h1 class="text-center">Benvenuto</h1>
+            </div>
+        </div>
+    </div>
 @endsection
 
 @section('page-title', 'Homepage - Laravel Crud')
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,3 +1,10 @@
 <header>
-    <h1>Header</h1>
+    <ul class="nav justify-content-center">
+        <li class="nav-item">
+            <a class="nav-link" href="{{ route('homepage') }}">Homepage</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Products</a>
+        </li>
+    </ul>
 </header>
