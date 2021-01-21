424bd03b3df64c899729ac3daca8f5fe4b683969
Rocco Chiarella <rocco.chiarella.rc@gmail.com>
2021-01-21 16:46:43 +0100

    Creato form html

--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -5,6 +5,23 @@
         <div class="row">
             <div class="col-12">
                 <h1 class="text-center">Nuovo prodotto</h1>
+                <form method="POST" action="{{route('products.store')}}">
+                    <div class="form-group">
+                        <label>Nome prodotto</label>
+                        <input type="text" name="name" class="form-control" placeholder="Tubo">
+                    </div>
+                    <div class="form-group">
+                        <label>Prezzo prodotto</label>
+                        <input type="text" name="price" class="form-control" placeholder="20€">
+                    </div>
+                    <div class="form-group">
+                        <label>Descrizione prodotto</label>
+                        <textarea class="form-control" name="description" rows="3"></textarea>
+                    </div>
+                    <div class="form-group">
+                        <button type="submit" class="btn btn-info">Crea prodotto</button>
+                    </div>
+                </form>
             </div>
         </div>
     </div>
