8212746a791fe6b848101ab91fe901a039c3792f
Rocco Chiarella <rocco.chiarella.rc@gmail.com>
2021-01-21 16:32:33 +0100

    Creata rotta al form per creare un nuovo prodotto

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -2,9 +2,16 @@
 
 @section('content')
     <div class="container">
+        <div class="row text-center">
+            <div class="col-6">
+                <h1 class="text-center">Tutti i prodotti</h1>
+            </div>
+            <div class="col-6" style="display: flex;align-items: flex-end;justify-content: flex-end;">
+                <a href="{{route('products.create')}}">Clicca qui per inserire un nuovo prodotto</a>
+            </div>
+        </div>
         <div class="row">
             <div class="col-12">
-                <h1 class="text-center">Tutti i prodotti</h1>
                 <table class="table text-center">
                     <thead>
                         <tr>
