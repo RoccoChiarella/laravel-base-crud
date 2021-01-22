ef38ea5bcb88143b99f9765dda24816e4b18bf01
Rocco Chiarella <rocco.chiarella.rc@gmail.com>
2021-01-22 16:12:06 +0100

    Eliminazione prodotto

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -28,6 +28,14 @@
                                 <td>
                                     <a href="{{route('products.show', ['product' => $product->id])}}" class="btn btn-info">Dettagli</a>
                                     <a href="{{ route('products.edit', ['product' => $product->id]) }}" class="btn btn-warning">Modifica</a>
+                                    <form method="POST" class="d-inline-block"
+                                    action="{{ route('products.destroy', ['product' => $product->id]) }}">
+                                        @csrf
+                                        @method('DELETE')
+                                        <button type="submit" class="btn btn-danger">
+                                            Cancella
+                                        </button>
+                                    </form>
                                 </td>
                             </tr>
                         @endforeach
